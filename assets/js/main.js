ebfcbfd1e84ff06cc0eb32afe87d7696c49726d0
Veltorie <65929922+Veltorie@users.noreply.github.com>
2025-10-01 00:20:12 -0400

    finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,59 @@
-/*==================== MENU SHOW Y HIDDEN ====================*/
+/*==================== const ====================*/
+var logosetting = 'LOGOBACKGROUND';
+var colourselected = [51,51,51];
+var logoset = "0" ;
+var LOGOBACKGROUND         = "0" ;
+var LOGOBACKGROUNDCOLOUR   = [51,51,51] ;
+var LOGOMIDDLEGROUND       = "0" ;
+var LOGOMIDDLEGROUNDCOLOUR = [51,51,51] ;
+var LOGOFORGROUND          = "0" ;
+var LOGOFORGROUNDCOLOUR    = [51,51,51] ;
 
+/*===== Functions =====*/
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-/*===== MENU SHOW =====*/
-/* Validate if constant exists */
+function switchgrounding(p1){
+    logosetting = p1;
+    if (p1 == 'LOGOBACKGROUND'){
+        logoset = LOGOBACKGROUND;
+        colourselected = LOGOBACKGROUNDCOLOUR;
+    } else if (p1 == 'LOGOMIDDLEGROUND'){
+        logoset = LOGOMIDDLEGROUND;
+        colourselected = LOGOMIDDLEGROUNDCOLOUR;
+    } else if (p1 == 'LOGOFORGROUND'){
+        logoset = LOGOFORGROUND;
+        colourselected = LOGOFORGROUNDCOLOUR;
+    }
+}
 
+async function updatecanvas() {
+    if (logosetting == 'LOGOBACKGROUND'){
+        LOGOBACKGROUND = logoset;
+        LOGOBACKGROUNDCOLOUR = colourselected;
+    } else if (logosetting == 'LOGOMIDDLEGROUND'){
+        LOGOMIDDLEGROUND = logoset;
+        LOGOMIDDLEGROUNDCOLOUR = colourselected;
+    } else if (logosetting == 'LOGOFORGROUND'){
+        LOGOFORGROUND = logoset;
+        LOGOFORGROUNDCOLOUR = colourselected;
+    }
+    document.getElementById("LOGOSETTING").src = `/assets/img/${logoset}.png`;
+    const c = document.getElementById(logosetting);
+    const c2nd = document.getElementById(`${logosetting}FINAL`);
+    const ctx = c.getContext("2d");
+    await sleep(50);
+    ctx.clearRect(0, 0, c.width, c.height);
+    ctx.drawImage(document.getElementById("LOGOSETTING"), 0, 0);
+    const imgData = ctx.getImageData(0, 0, c.width, c.height);
 
-/*===== MENU HIDDEN =====*/
-/* Validate if constant exists */
-
-
-/*==================== REMOVE MENU MOBILE ====================*/
-
-
-/*==================== ACCORDION SKILLS ====================*/
-
-
-/*==================== QUALIFICATION TABS ====================*/
-
-
-/*==================== SERVICES MODAL ====================*/
-
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-
-/*==================== TESTIMONIAL ====================*/
-
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
-
-/*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
-
-/*==================== SHOW SCROLL UP ====================*/ 
-
-
-/*==================== DARK LIGHT THEME ====================*/ 
+    for (let i = 0; i < imgData.data.length; i += 4) {
+        imgData.data[i] = colourselected[0];
+        imgData.data[i+1] = colourselected[1];
+        imgData.data[i+2] = colourselected[2];
+    }
+    ctx.putImageData(imgData, 0, 0);
+    c2nd.getContext("2d").clearRect(0, 0, c2nd.width, c2nd.height);
+    c2nd.getContext("2d").putImageData(imgData, 0, 0);
+}
